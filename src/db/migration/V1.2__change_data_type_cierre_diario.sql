4c1c05aa9c91d67f6af1bb5aad2408d357dd3661
Rhonal <rhonalchirinos@gmail.com>
2017-10-26 09:33:58 -0400

    cambio del tipo de datos, tabla cierre diario

---
+++ src/db/migration/V1.2__change_data_type_cierre_diario.sql
@@ -0,0 +1,8 @@
+alter table administracion.tbl_dem_cierrediario
+ALTER COLUMN  dbl_ingfac TYPE numeric(20,4);
+
+alter table administracion.tbl_dem_cierrediario
+ALTER COLUMN  dbl_impfac TYPE numeric(20,4);
+
+alter table administracion.tbl_dem_cierrediario
+ALTER COLUMN  dbl_cuecob TYPE numeric(20,4);
